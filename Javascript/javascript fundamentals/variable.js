dbe813bdc3a90ca846c7dce9df35a7e490dd1d0f
vermaayush84300@gmail.com <vermaayush84300@gmail.com>
2026-03-20 17:44:52 +0530

    javascript

---
+++ Javascript/javascript fundamentals/variable.js
@@ -0,0 +1,40 @@
+// var ,let cons are the three ways to declare a variable in javascript
+
+// var is function scoped and can be re-declared and updated
+// var name = "Ayush";
+// var age = 30;
+// var isStudent = true;
+
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+// var age = 15;
+// console.log(age);
+// let name = "Ayush";
+// let age = 30;
+// let isStudent = true;
+
+// console.log(name);
+// console.log(age);
+// console.log(isStudent);
+
+// if (true) {
+//    var age = 15;
+// }
+// console.log(age);
+
+// if (true) {
+//     let age = 15;
+// }
+// console.log(age);
+
+// const PI= 3.14;
+// console.log(PI);
+// PI = 3.14159; // This will throw an error because PI is a constant and cannot be reassigned
+// console.log(PI);
+// console.log(typeof PI);
+
+let name = "Ayush";
+let age1 = 18;
+console.log("my name is " + name + " and my age is " + age1);
+console.log(`my name is ${name} and my age is ${age1}`);
