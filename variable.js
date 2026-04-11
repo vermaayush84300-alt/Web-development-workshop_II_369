515346deab4caa714831db43b47fa520a8f93695
vermaayush84300@gmail.com <vermaayush84300@gmail.com>
2026-04-11 20:12:45 +0530

    using of variable

---
+++ variable.js
@@ -0,0 +1,47 @@
+let name = "anil";
+let data = ("city name" + "lucknow");
+console.log(data, name)
+console.log(typeof name)
+
+// variable = a container that can store a value.
+// behave as if it were the value it contains.
+
+let x = 5;
+let y = 10;
+let sum = x + y;
+console.log(sum);
+
+let age = 25;
+console.log(age);
+age = 30;
+console.log(age);
+let price = 19.99;
+console.log(price);
+price = 29.99;
+console.log(price);
+let gpa = 9.26666666;
+console.log(gpa);
+gpa = 8.5;
+console.log(gpa);
+
+console.log(`you are ${age} years old`);
+console.log("you are " + age + " years old");
+console.log(typeof age);
+console.log(typeof price);
+console.log(typeof gpa);
+
+
+let firstName = "ayush";
+let lastName = "kumar";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+
+// boolen;
+let online = true;
+let offline = false;
+console.log(online);
+console.log(offline);
+console.log(typeof online);
+console.log(typeof offline);
+
