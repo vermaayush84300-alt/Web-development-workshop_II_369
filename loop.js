3f4b1c149ee8d7846e9b5d722192ca3b63aa0bbc
vermaayush84300-alt <vermaayush84300@gmail.com>
2026-05-15 11:22:05 +0530

    loop

---
+++ loop.js
@@ -0,0 +1,16 @@
+let = n
+n = prompt("Enter a number: ");
+for (let i = 0; i <= n; i++) {
+    console.log("hello world");
+}
+console.log("hello javascript")
+
+for (let i = 2; i <= n; i++) {
+    if (i == 13) {
+        break
+    }
+    else {
+        console.log(i);
+    }
+}
+
