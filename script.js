e3d44f7beffc484002d9acddcbbbf72910064239
vermaayush84300-alt <vermaayush84300@gmail.com>
2026-05-15 11:23:41 +0530

    basic js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-const textbox = document.getElementById("textbox");
-const toFahrenhiet = document.getElementById("toFahrenhiet");
-const toCelcius = document.getElementById("toCelcius");
-const result = document.getElementById("result");
-let temp;
+let username;
+username = prompt("What is your name?");
+alert("Hello " + username + "!");
+console.log(username);
 
-function convert() {
-    if (toFahrenhiet.checked) {
-        temp = (Number(textbox.value) * 9) / 5 + 32;
-        result.textContent = "" + textbox.value + "°C is equal to " + temp + "°F";
-    } else if (toCelcius.checked) {
-        temp = ((Number(textbox.value) - 32) * 5) / 9;
-        result.textContent = "" + textbox.value + "°F is equal to " + temp + "°C";
-    } else {
-        result.textContent = "select a unit";
-    }
-    if(toKalvin.checked){
-        temp = (Number(textbox.value)+273);
-        result.textContent = "" + textbox.value + "°C is equal to" + temp + "k"    }
-
+document.getElementById("submit").onclick = function () {
+    username = document.getElementById("nameInput").value;
+    document.getElementById("myh1").textContent = "Hello " + username + "!";
+    alert("Hello " + username + "!");
+    console.log(username);
 }
+
